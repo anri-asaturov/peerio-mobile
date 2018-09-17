34b5496262e7989de9a65ce4238d58ae3acfcbda
Karim94 <karimchehab94@gmail.com>
2018-09-17 14:34:19 -0400

    chore: mock beacons

--- app/components/mocks/mock-beacon.js
+++ app/components/mocks/mock-beacon.js
@@ -1,53 +1,76 @@
 import React, { Component } from 'react';
+import { action, observable } from 'mobx';
 import { View, StatusBar } from 'react-native';
 import { observer } from 'mobx-react/native';
+import Text from '../controls/custom-text';
 import PopupLayout from '../layout/popup-layout';
-import ChatList from '../messaging/chat-list';
-import ContactList from '../contacts/contact-list';
-import SettingsLevel1 from '../settings/settings-level-1';
-import Files from '../files/files';
-import ChannelInvite from '../messaging/channel-invite';
 import { User } from '../../lib/icebear';
-import chatState from '../messaging/chat-state';
 import contactState from '../contacts/contact-state';
-import mockChatStore from './mock-chat-store';
 import { vars } from '../../styles/styles';
 import mockContactStore from './mock-contact-store';
-import mockFileStore from './mock-file-store';
-import TabContainer from '../layout/tab-container';
 import BeaconLayout from '../beacons/beacon-layout';
 import routes from '../routes/routes';
+import buttons from '../helpers/buttons';
+import MockTabContainer from './mock-tab-container';
+import { uiState } from '../states';
 
 @observer
 export default class MockBeacon extends Component {
+    @observable showVertical = false;
+    @observable showTop = false;
+    @observable showLeft = false;
+
     componentWillMount() {
         User.current = mockContactStore.createMock();
         User.current.activePlans = [];
-        mockFileStore.install();
-        chatState.store = mockChatStore;
-        chatState.init();
         contactState.store = mockContactStore;
         contactState.init();
-        routes.main.route = 'contacts';
+        routes.main.route = 'chats';
+    }
+
+    @action.bound toggleTopBottom() {
+        this.showVertical = true;
+        this.showTop = !this.showTop;
+        this.showLeft = false;
+    }
+
+    @action.bound toggleLeftRight() {
+        this.showVertical = false;
+        this.showLeft = !this.showLeft;
+        this.showTop = false;
+    }
+
+    @action.bound toggleArrow() {
+        uiState.mockBeaconArrowDirection = !uiState.mockBeaconArrowDirection;
+    }
+
+    @action.bound toggleBeaconType() {
+        uiState.mockBeaconType = !uiState.mockBeaconType;
     }
 
     get list() {
-        const { route } = routes.main;
-        switch (route) {
-            case 'channelInviteList': return <ChannelInvite />;
-            case 'settings': return <SettingsLevel1 />;
-            case 'chats': return <ChatList />;
-            case 'contacts': return <ContactList />;
-            case 'files': return <Files />;
-            default: return null;
-        }
+        return (
+            <View style={{ flexGrow: 1, backgroundColor: vars.lightGrayBg, flexDirection: 'row' }}>
+                {!this.showVertical && this.showLeft && <MockTabContainer vertical />}
+                <View style={{ flex: 1, flexGrow: 1, backgroundColor: vars.white, paddingHorizontal: 20, paddingVertical: 50 }}>
+                    <Text>1. Click on any icon to see its beacon.</Text>
+                    <Text>2. Use the blue buttons to change the location of the icons</Text>
+                    <Text>3. Use the white buttons to change change beacon properties (must refresh by pressing any blue button afterwards)</Text>
+                    {buttons.roundBlueBgButton('Toggle Top/Bottom Beacons', this.toggleTopBottom)}
+                    {buttons.roundBlueBgButton('Toggle Left/Right Beacons', this.toggleLeftRight)}
+                    {buttons.roundWhiteBgButton('Toggle arrow orientation', this.toggleArrow)}
+                    {buttons.roundWhiteBgButton('Toggle Area/Spot beacon', this.toggleBeaconType)}
+                </View>
+                {!this.showVertical && !this.showLeft && <MockTabContainer vertical />}
+            </View>);
     }
 
     render() {
         return (
             <View style={{ backgroundColor: 'white', flex: 1, flexGrow: 1, paddingTop: vars.layoutPaddingTop }}>
+                {this.showVertical && this.showTop && <MockTabContainer />}
                 {this.list}
-                <TabContainer />
+                {this.showVertical && !this.showTop && <MockTabContainer />}
                 <PopupLayout key="popups" />
                 <StatusBar barStyle="default" />
                 <BeaconLayout />
