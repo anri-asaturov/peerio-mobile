90dcaece68a88089fd331b9ad6b6c56ffba0c1ea
Alexandra Marin <alexandra.marin01@gmail.com>
2019-01-03 21:05:39 +0200

    WIP: type icon

--- app/components/files/file-type-icon.tsx
+++ app/components/files/file-type-icon.tsx
@@ -1,9 +1,14 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import { Image, View } from 'react-native';
 import { vars } from '../../styles/styles';
+import SafeComponent from '../shared/safe-component';
 
-export default class FileTypeIcon extends React.Component {
+export interface FileTypeIconProps {
+    type: string;
+    size: string;
+}
+
+export default class FileTypeIcon extends SafeComponent<FileTypeIconProps> {
     /* props
         size: small, medium, large
         type: img, audio, video, txt, zip, pdf, ai, psd, word, xls, ppt, other
@@ -71,8 +76,3 @@ export default class FileTypeIcon extends React.Component {
         return iconSource;
     }
 }
-
-FileTypeIcon.propTypes = {
-    type: PropTypes.string,
-    size: PropTypes.string
-};
